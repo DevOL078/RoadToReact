7bce1d18bff83de42953c6f63790d53cbd260e98
DevOL78 <oleinik.dima098@mail.ru>
2020-09-29 22:34:33 +0300

    React JSX

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 
-const title = 'React';
+function getTitle(title) {
+  return title;
+}
 
 function App() {
 
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>{getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
