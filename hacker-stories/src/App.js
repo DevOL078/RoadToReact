a8429868a98dd950ce63d70f15a95a666c2063d4
DevOL78 <oleinik.dima098@mail.ru>
2020-09-30 22:29:22 +0300

    Handler Function in JSX

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -21,13 +21,16 @@ const list = [
 ];
 
 const App = () => {
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange}/>
 
       <hr />
 
@@ -36,8 +39,8 @@ const App = () => {
   );
 }
 
-const List = () => {
-  return list.map(item => {
+const List = () =>
+  list.map(item => {
     return (
       <div key={item.objectID}>
         <span>
@@ -49,6 +52,5 @@ const List = () => {
       </div>
     );
   });
-}
 
 export default App;
