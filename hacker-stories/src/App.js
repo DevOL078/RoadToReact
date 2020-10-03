4c703ec49b9cde6ad6a94144a168ba26636ba343
DevOL78 <oleinik.dima098@mail.ru>
2020-10-03 23:34:43 +0300

    Data Fetching in React

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,33 +1,7 @@
 import React from 'react';
 import './App.css';
 
-const intialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () => {
-  return new Promise((resolve) =>
-    setTimeout(
-      () => resolve({data: {stories: intialStories } }),
-      2000
-    )
-  );
-};
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -87,11 +61,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then(response => response.json())
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() =>
